4a9a73e1d0847c6653df8d7ce6392fee80f771aa
jerrylaikr <kerui.lai@mail.mcgill.ca>
2021-10-02 04:03:18 +0000

    Implement customized vasp input sets

---
+++ src/ph3pywf/utils/set.py
@@ -0,0 +1,36 @@
+from pymatgen.io.vasp.sets import _load_yaml_config, DictSet
+from pymatgen.io.vasp.inputs import Incar, Kpoints, Poscar, Potcar, VaspInput
+
+MODULE_DIR = Path(__file__).resolve().parent
+
+class Ph3pyRelaxSet(DictSet):
+    """
+    Implementation of VaspInputSet for relaxation jobs in Ph3py workflow. 
+    """
+
+    CONFIG = _load_yaml_config("Ph3pyRelaxSet")
+
+    def __init__(self, structure, **kwargs):
+        """
+        :param structure: Structure
+        :param kwargs: Same as those supported by DictSet.
+        """
+        super().__init__(structure, Ph3pyRelaxSet.CONFIG, **kwargs)
+        self.kwargs = kwargs
+
+
+class Ph3pyStaticSet(DictSet):
+    """
+    Implementation of VaspInputSet for static jobs in Ph3py workflow. 
+    """
+
+    CONFIG = _load_yaml_config("Ph3pyStaticSet")
+
+    def __init__(self, structure, **kwargs):
+        """
+        :param structure: Structure
+        :param kwargs: Same as those supported by DictSet.
+        """
+        super().__init__(structure, Ph3pyRelaxSet.CONFIG, **kwargs)
+        self.kwargs = kwargs
+        
