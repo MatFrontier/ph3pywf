7a6159d8e8ce6fc54363a2a9e5dcd4c253a2fd51
jerrylaikr <kerui.lai@mail.mcgill.ca>
2021-10-02 18:43:19 +0000

    update Ph3pyStaticSet to fix lepsilon argument issue

--- src/ph3pywf/utils/sets.py
+++ src/ph3pywf/utils/sets.py
@@ -42,11 +42,73 @@ class Ph3pyStaticSet(DictSet):
 
     CONFIG = _load_yaml_config("Ph3pyStaticSet")
 
-    def __init__(self, structure, **kwargs):
+    def __init__(
+        self, 
+        structure,
+        lepsilon=False,
+        lcalcpol=False,
+        **kwargs):
         """
-        :param structure: Structure
-        :param kwargs: Same as those supported by DictSet.
+        Args:
+            structure (Structure): Structure from previous run.
+            lepsilon (bool): Whether to add static dielectric calculation.
+            lcalcpol (bool): Whether to turn on evaluation of the Berry phase approximations
+                for electronic polarization
+            **kwargs: kwargs supported by MPRelaxSet.
         """
-        super().__init__(structure, Ph3pyRelaxSet.CONFIG, **kwargs)
+        super().__init__(structure, Ph3pyStaticSet.CONFIG, **kwargs)
         self.kwargs = kwargs
+        self.lepsilon = lepsilon
+        self.lcalcpol = lcalcpol
         
+    @property
+    def incar(self):
+        """
+        :return: Incar
+        """
+        parent_incar = super().incar
+
+        if self.lepsilon:
+            incar["IBRION"] = 8
+            incar["LEPSILON"] = True
+
+            # LPEAD=T: numerical evaluation of overlap integral prevents
+            # LRF_COMMUTATOR errors and can lead to better expt. agreement
+            # but produces slightly different results
+            incar["LPEAD"] = True
+
+            # Note that DFPT calculations MUST unset NSW. NSW = 0 will fail
+            # to output ionic.
+            incar.pop("NSW", None)
+            incar.pop("NPAR", None)
+
+            # tighter ediff for DFPT
+            incar["EDIFF"] = 1e-5
+
+        if self.lcalcpol:
+            incar["LCALCPOL"] = True
+
+        for k in ["MAGMOM", "NUPDOWN"] + list(self.user_incar_settings.keys()):
+            # For these parameters as well as user specified settings, override
+            # the incar settings.
+            if parent_incar.get(k, None) is not None:
+                incar[k] = parent_incar[k]
+            else:
+                incar.pop(k, None)
+
+        # use new LDAUU when possible b/c the Poscar might have changed
+        # representation
+        if incar.get("LDAU"):
+            u = incar.get("LDAUU", [])
+            j = incar.get("LDAUJ", [])
+            if sum([u[x] - j[x] for x, y in enumerate(u)]) > 0:
+                for tag in ("LDAUU", "LDAUL", "LDAUJ"):
+                    incar.update({tag: parent_incar[tag]})
+            # ensure to have LMAXMIX for GGA+U static run
+            if "LMAXMIX" not in incar:
+                incar.update({"LMAXMIX": parent_incar["LMAXMIX"]})
+
+        # Compare ediff between previous and staticinputset values,
+        # choose the tighter ediff
+        incar["EDIFF"] = min(incar.get("EDIFF", 1), parent_incar["EDIFF"])
+        return incar
