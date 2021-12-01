69a59cb0b918266179e6d76341c7251c7ef2c7b6
jerrylaikr <kerui.lai@mail.mcgill.ca>
2021-12-01 03:51:16 +0000

    change sleep time

--- src/ph3pywf/utils/check_progress_and_rerun.py
+++ src/ph3pywf/utils/check_progress_and_rerun.py
@@ -9,21 +9,25 @@ import subprocess
 from ph3pywf.utils.mission_control import check_progress_and_rerun
 
 def main():
-    tag = sys.argv[1]
+    tag = sys.argv[1] # TODO: change this to use fw_id in the future
     user = "jerrylai"
     # if len(sys.argv) == 3:
     #     user = sys.argv[2]
-        
+    
+    sleep_time = 3600
+    
     while True:
         sq = subprocess.run(["squeue","-u",user], capture_output=True, text=True).stdout
         n_jobs = sq.count("\n") - 1
         print(f"{n_jobs} jobs running")
         if n_jobs == 0:
-            print("no slurm job running")
+            print("no slurm job running, exiting")
             break
         
         check_progress_and_rerun(tag)
-        time.sleep(30)
+        
+        print(f"sleep for {sleep_time} seconds")
+        time.sleep(sleep_time)
     
 
 if __name__ == "__main__":
