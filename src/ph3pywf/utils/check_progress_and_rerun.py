8e5e46684888226e6f66626486ae7aa58bebc035
jerrylaikr <kerui.lai@mail.mcgill.ca>
2021-12-10 04:09:51 +0000

    add TODOs

--- src/ph3pywf/utils/check_progress_and_rerun.py
+++ src/ph3pywf/utils/check_progress_and_rerun.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import subprocess
 from ph3pywf.utils.mission_control import check_progress_and_rerun
 
+# TODO: make this run locally without connected to McGill vpn
 def main():
     tag = sys.argv[1] # TODO: change this to use fw_id in the future
     user = "jerrylai"
