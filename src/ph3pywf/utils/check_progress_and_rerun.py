4b146cf5404b36692612ecdd63ec8b87352d7fca
jerrylaikr <kerui.lai@mail.mcgill.ca>
2022-01-06 22:02:18 +0000

    change check progress to run only once when called, this is no longer designed to run in background

--- src/ph3pywf/utils/check_progress_and_rerun.py
+++ src/ph3pywf/utils/check_progress_and_rerun.py
@@ -4,34 +4,18 @@ __author__ = "Kerui Lai"
 __email__ = "kerui.lai@mail.mcgill.ca"
 
 import sys
-import time
+# import time
 from datetime import datetime
-import subprocess
+# import subprocess
 from ph3pywf.utils.mission_control import check_progress_and_rerun
 
-# TODO: make this run locally without connected to McGill vpn
 def main():
     tag = sys.argv[1] # TODO: change this to use fw_id in the future
     user = "jerrylai"
     # if len(sys.argv) == 3:
     #     user = sys.argv[2]
     
-    sleep_time = 1200
-    
-    while True:
-        print(datetime.utcnow())
-        
-        sq = subprocess.run(["squeue","-u",user], capture_output=True, text=True).stdout
-        n_jobs = sq.count("\n") - 1
-        print(f"{n_jobs} jobs running")
-        if n_jobs == 0:
-            print("no slurm job running, exiting")
-            break
-        
-        check_progress_and_rerun(tag)
-        
-        print(f"sleep for {sleep_time} seconds")
-        time.sleep(sleep_time)
+    check_progress_and_rerun(tag)
     
 
 if __name__ == "__main__":
