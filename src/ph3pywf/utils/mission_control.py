6973d8e498d54b1c3e9deb28678d3fd959746224
jerrylaikr <kerui.lai@mail.mcgill.ca>
2022-05-12 23:30:16 +0000

    add post analysis plotter utility

--- src/ph3pywf/utils/mission_control.py
+++ src/ph3pywf/utils/mission_control.py
@@ -8,45 +8,49 @@ from fireworks.fw_config import LAUNCHPAD_LOC
 from atomate.vasp.database import VaspCalcDb
 import os
 
+
 def check_progress_and_rerun(tag):
-    
+
     launchpad = LaunchPad.auto_load()
-    
-    # Check progress 
-    RUNNING_ids = launchpad.get_fw_ids(
-        {"state": "RUNNING", "name": {"$regex": tag}})
+
+    # Check progress
+    RUNNING_ids = launchpad.get_fw_ids({"state": "RUNNING", "name": {"$regex": tag}})
     COMPLETED_ids = launchpad.get_fw_ids(
-        {"state": "COMPLETED", "name": {"$regex": tag}})
-    FIZZLED_ids = launchpad.get_fw_ids(
-        {"state": "FIZZLED", "name": {"$regex": tag}})
+        {"state": "COMPLETED", "name": {"$regex": tag}}
+    )
+    FIZZLED_ids = launchpad.get_fw_ids({"state": "FIZZLED", "name": {"$regex": tag}})
     ALL_ids = launchpad.get_fw_ids({"name": {"$regex": tag}})
 
-    print(f"total: {len(ALL_ids)}\nRUNNING: {len(RUNNING_ids)}\nFIZZLED: {len(FIZZLED_ids)}\nCOMPLETED: {len(COMPLETED_ids)}")
+    print(
+        f"total: {len(ALL_ids)}\nRUNNING: {len(RUNNING_ids)}\nFIZZLED: {len(FIZZLED_ids)}\nCOMPLETED: {len(COMPLETED_ids)}"
+    )
 
     # Rerun FIZZLED
     for fw_id in FIZZLED_ids:
         d = launchpad.get_fw_dict_by_id(fw_id)
         n_archived_launches = len(d["archived_launches"])
-        
+
         # print a warning if a FW has failed more than 1 time
         if n_archived_launches > 1:
             print(f"WARNING: fw: {fw_id}, has FIZZLED {n_archived_launches} times")
-        
+
         print(f"INFO: rerunning FIZZLED fw: {fw_id}")
         launchpad.rerun_fw(fw_id)
 
     # Rerun lost runs
-    LOST_ids = launchpad.detect_lostruns(expiration_secs=1*60*60,
-                                         query={"name": {"$regex": tag}})[1]
+    LOST_ids = launchpad.detect_lostruns(
+        expiration_secs=1 * 60 * 60, query={"name": {"$regex": tag}}
+    )[1]
     for fw_id in LOST_ids:
         print(f"INFO: rerunning lost run: {fw_id}")
         launchpad.rerun_fw(fw_id)
 
     print("DONE")
 
+
 def check_time_of_each_run(tag):
     db_file = os.path.join(os.path.dirname(LAUNCHPAD_LOC), "db.json")
-    
+
     mmdb = VaspCalcDb.from_db_file(db_file)
     docs_p = mmdb.collection.find(
         {
@@ -59,16 +63,25 @@ def check_time_of_each_run(tag):
         docs.append(p)
     # print(type(docs[0]))
     for d in docs:
-        print("{{task_label:\"{}\"}}".format(d["task_label"]))
+        print('{{task_label:"{}"}}'.format(d["task_label"]))
         if "nsites" in d:
             print("nsites = {}".format(d["nsites"]))
         if "input" in d:
             print("EDIFF = {}".format(d["input"]["incar"]["EDIFF"]))
         if "run_stats" in d:
             if "standard" in d["run_stats"]:
-                print("Maximum memory used (kb) = {}".format(d["run_stats"]["standard"]["Maximum memory used (kb)"]))
-            print("Elapsed time (sec) = {}".format(d["run_stats"]["overall"]["Elapsed time (sec)"]))
+                print(
+                    "Maximum memory used (kb) = {}".format(
+                        d["run_stats"]["standard"]["Maximum memory used (kb)"]
+                    )
+                )
+            print(
+                "Elapsed time (sec) = {}".format(
+                    d["run_stats"]["overall"]["Elapsed time (sec)"]
+                )
+            )
         print("")
+
 
 def get_tag_from_fw_id(fw_id):
     launchpad = LaunchPad.auto_load()
@@ -79,20 +92,20 @@ def get_tag_from_fw_id(fw_id):
 
 def get_resource_report(tag):
     db_file = os.path.join(os.path.dirname(LAUNCHPAD_LOC), "db.json")
-    
+
     mmdb = VaspCalcDb.from_db_file(db_file)
     docs_p = mmdb.collection.find(
         {
             "task_label": {"$regex": f"{tag}"},
         }
     )
-    
+
     docs = []
     print("Fetching tasks...")
     for p in docs_p:
         docs.append(p)
     # print(type(docs[0]))
-    
+
     n_tasks = 0
     total_time = 0
     total_mem_used = 0
@@ -102,13 +115,13 @@ def get_resource_report(tag):
             if "standard" in d["run_stats"]:
                 total_mem_used += d["run_stats"]["standard"]["Maximum memory used (kb)"]
                 cores = d["run_stats"]["standard"]["cores"]
-                cores_used[cores] = cores_used[cores]+1 if cores in cores_used else 0
+                cores_used[cores] = cores_used[cores] + 1 if cores in cores_used else 0
             total_time += d["run_stats"]["overall"]["Elapsed time (sec)"]
         n_tasks += 1
-    
+
     print("Total number of tasks = {}".format(n_tasks))
-    print("Total computation time (h) = {}".format(total_time/3600))
-    print("Average time per task (s) = {}".format(total_time/n_tasks))
-    print("Average memory used (kb) = {}".format(total_mem_used/n_tasks))
+    print("Total computation time (h) = {}".format(total_time / 3600))
+    print("Average time per task (s) = {}".format(total_time / n_tasks))
+    print("Average memory used (kb) = {}".format(total_mem_used / n_tasks))
     for cores, count in cores_used.items():
         print(f"{count} tasks used {cores} cores")
---
+++ src/ph3pywf/utils/post_analysis.py
@@ -0,0 +1,160 @@
+# coding: utf-8
+
+__author__ = "Kerui Lai"
+__email__ = "kerui.lai@mail.mcgill.ca"
+
+from atomate.vasp.database import VaspCalcDb
+from pymatgen.core import Structure
+import numpy as np
+import matplotlib.pyplot as plt
+from pymatgen.phonon.bandstructure import PhononBandStructureSymmLine
+from pymatgen.phonon.dos import CompletePhononDos
+from pymatgen.phonon.plotter import PhononBSPlotter, PhononDosPlotter
+import csv
+
+
+class DocNotFoundError(Exception):
+    pass
+
+
+class Ph3py_Result:
+    def __init__(
+        self,
+        task_label,
+        path_to_db_json,
+    ):
+        self.has_fitted = False
+
+        # connect to DB
+        atomate_db = VaspCalcDb.from_db_file(path_to_db_json)
+
+        # use the Ph3py collection
+        ph3py_coll = atomate_db.db["ph3py_tasks"]
+
+        # get our task
+        self.ph3py_entry = ph3py_coll.find_one({"task_label": task_label})
+        if bool(self.ph3py_entry):
+            raise DocNotFoundError(f"Unable to find doc from DB using {task_label = }")
+
+        self.formula_pretty = self.ph3py_entry["formula_pretty"]
+
+        # set up the pymatgen structure
+        self.struct_unitcell = Structure.from_dict(self.ph3py_entry["structure"])
+
+        # extract thermal conductivity calculation result (initial)
+        self.T_calc = np.array(self.ph3py_entry["temperature"])
+        self.kappa_calc = np.array(self.ph3py_entry["kappa"])
+        self.kappa_calc_iso = np.mean(self.kappa_calc[:, :3], axis=1)
+
+        # extract thermal conductivity calculation result (fitted) if available
+        if "temperature_fitted" in self.ph3py_entry:
+            self.has_fitted = True
+            self.T_calc_fitted = np.array(self.ph3py_entry["temperature_fitted"])
+            self.kappa_calc_fitted = np.array(self.ph3py_entry["kappa_fitted"])
+            self.kappa_calc_iso_fitted = np.mean(self.kappa_calc_fitted[:, :3], axis=1)
+
+        # extract band structure
+        self.bs = PhononBandStructureSymmLine.from_dict(
+            self.ph3py_entry["band_structure"]
+        )
+
+        # extract dos
+        self.dos = CompletePhononDos.from_dict(self.ph3py_entry["dos"])
+
+    def plot_thermal_conductivity(
+        self,
+        ref_filenames=[],
+        ref_labels=[],
+        plot_dircs=False,
+        plot_fitted=True,
+        save_file=True,
+    ):
+        # plot params
+        plt.rcParams["font.family"] = "Times New Roman"
+        plt.rcParams["font.size"] = 12
+
+        # get reference results
+        T_exp = []
+        kappa_exp = []
+        for filename in ref_filenames:
+            T_exp.append([])
+            kappa_exp.append([])
+            with open(filename, newline="") as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    T_exp[-1].append(float(row[0]))
+                    kappa_exp[-1].append(float(row[1]))
+
+        # plot directional calculated kappa (anisotropic)
+        if plot_dircs:
+            for dirc in range(3):
+                plt.plot(
+                    self.T_calc,
+                    self.kappa_calc[:, dirc],
+                    "b:",
+                    label=f"Calculated dir{dirc} (initial)",
+                )
+
+        # plot calculated kappa (initial)
+        plt.plot(
+            self.T_calc,
+            self.kappa_calc_iso,
+            "b--",
+            label="Calculated isotropic thermal conductivity (initial)",
+        )
+
+        # plot fitted
+        if plot_fitted and self.has_fitted:
+            plt.plot(
+                self.T_calc_fitted,
+                self.kappa_calc_iso_fitted,
+                "b-",
+                label="Calculated isotropic thermal conductivity (fitted) (monoclinic)",
+            )
+
+        # plot experiment results
+        for i in range(len(self.T_exp)):
+            plt.scatter(self.T_exp[i], self.kappa_exp[i], label=ref_labels[i])
+
+        # figure settings
+        plt.title("Thermal conductivity of {}".format(self.formula_pretty))
+        # plt.xscale("log") # LOG SCALE
+        # plt.yscale("log") # LOG SCALE
+        plt.xlabel("T (K)")
+        plt.ylabel("Thermal conductivity (W/m-K)")
+        # plt.xlim(left=200)
+        plt.ylim(bottom=0, top=None)
+        plt.legend()
+
+        # save the figure
+        if save_file:
+            plt.savefig(
+                "{}-heat-conductivity-{}.png".format(
+                    self.formula_pretty, self.task_label
+                )
+            )
+
+        # show plot
+        plt.show()
+
+    def plot_bs(
+        self,
+        save_file=True,
+    ):
+        plotter = PhononBSPlotter(self.bs)
+        plotter.show()
+        if save_file:
+            plotter.save_plot(
+                "{}-phonon-dispersion-{}.png".format(self.formula_pretty, self.task_label), "png"
+            )
+
+    def plot_dos(
+        self,
+        save_file=True,
+    ):
+        plotter = PhononDosPlotter()
+        plotter.add_dos("Total DOS", self.dos)
+        plotter.add_dos_dict(self.dos.get_element_dos())
+        plotter.show(units="cm-1")
+        if save_file:
+            plotter.save_plot("{}-phonon-DOS-{}.png".format(self.formula_pretty, self.task_label),"png",units="cm-1")
